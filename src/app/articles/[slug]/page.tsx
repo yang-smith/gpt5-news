91751c436b4e16d9d8d77bc2944ff9d1b1dd7790
yang <zy892065502@gmail.com>
2024-09-12 12:21:12 +0800

    fix bug

--- src/app/articles/[slug]/page.tsx
+++ src/app/articles/[slug]/page.tsx
@@ -1,16 +1,16 @@
-import { getArticleContent } from '@/lib/api';
-import ArticleDisplay from '@/components/ArticleDisplay';
+// // import { getArticleContent } from '@/lib/api';
+// import ArticleDisplay from '@/components/ArticleDisplay';
 
-interface ArticlePageProps {
-  params: { slug: string };
-}
+// interface ArticlePageProps {
+//   params: { slug: string };
+// }
 
-export default async function ArticlePage({ params }: ArticlePageProps) {
-  const content = await getArticleContent(params.slug);
+// export default async function ArticlePage({ params }: ArticlePageProps) {
+//   const content = await getArticleContent(params.slug);
 
-  return (
-    <div className="container mx-auto px-4 py-8">
-      <ArticleDisplay content={content} />
-    </div>
-  );
-}
+//   return (
+//     <div className="container mx-auto px-4 py-8">
+//       <ArticleDisplay content={content} />
+//     </div>
+//   );
+// }
