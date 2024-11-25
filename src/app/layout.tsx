8ad163e0564a2d7886d93d39847780006ecd8c6f
yang <zy892065502@gmail.com>
2024-11-25 16:12:52 +0800

    add google adsense

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,6 +41,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8638022674571300"
+          crossOrigin="anonymous"></script>
+      </head>
       <body>{children}</body>
       <Analytics />
     </html>
